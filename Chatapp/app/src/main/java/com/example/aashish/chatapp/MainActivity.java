520552ab06d5d3f783904dd86d154c1988414eb5
AK-98 <37804913+Aashish792@users.noreply.github.com>
2018-08-02 21:34:44 +0545

    Add files via upload

---
+++ Chatapp/app/src/main/java/com/example/aashish/chatapp/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.aashish.chatapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private DatabaseReference myDatabase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myDatabase = FirebaseDatabase.getInstance().getReference("Message");
+
+        final TextView myText = findViewById(R.id.text);
+
+
+
+        myDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                myText.setText(dataSnapshot.getValue().toString());
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+                myText.setText("CANCELLED");
+
+            }
+        });
+
+    }
+
+    public void sendMessage(View view){
+        EditText myEditText = findViewById(R.id.editText);
+
+        myDatabase.setValue(myEditText.getText());
+        myEditText.setText(" ");
+
+
+
+    }
+}
